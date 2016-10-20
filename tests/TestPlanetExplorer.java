476026523cf09d334b33673077946de168e57678
somename <someemail>
2016-10-20 14:51:04 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,7 +8,7 @@ public class TestPlanetExplorer {
 	public void testThePlanet() {
 		PlanetExplorer planet = new PlanetExplorer(100,100,"");
 		
-		assertEquals(100, planet.getSize());
+		assertEquals(100*100, planet.getSize()*planet.getSize());
 		
 	}
 }
