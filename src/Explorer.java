ab9d7041caa96d2e4864728c4ae0c9bf0e9b78e2
somename <someemail>
2016-10-20 15:12:40 +0300

    besouro automatic message

--- src/Explorer.java
+++ src/Explorer.java
@@ -20,15 +20,5 @@ public class Explorer {
 	public int getY() {
 		return y;
 	}
-	public void setY(int y) {
-		this.y = y;
-	}
-	public String getDirection() {
-		return direction;
-	}
-	public void setDirection(String direction) {
-		this.direction = direction;
-	}
-	
 	
 }
